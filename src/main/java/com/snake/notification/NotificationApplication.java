4a78e206b3c4a9daea1c085178303c2bbd37720b
hungnmvettel <hungnm.vettel@gmail.com>
2025-06-26 10:31:39 +0700

    config

--- src/main/java/com/snake/notification/NotificationApplication.java
+++ src/main/java/com/snake/notification/NotificationApplication.java
@@ -1,4 +1,4 @@
-package com.example.notification;
+package com.snake.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
