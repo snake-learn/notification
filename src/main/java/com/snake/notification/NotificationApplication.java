9059bb4a00bf2fb06929209ca913d9ca89f8336a
hungnmvettel <hungnm.vettel@gmail.com>
2025-06-27 17:39:34 +0700

    log

--- src/main/java/com/snake/notification/NotificationApplication.java
+++ src/main/java/com/snake/notification/NotificationApplication.java
@@ -3,7 +3,7 @@ package com.snake.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.snake")
 public class NotificationApplication {
 
 	public static void main(String[] args) {
